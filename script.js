2bdcc7a303eefbf0a4780213f7a3c68af65a0b33
Artem Zabirov <peerlessgodlike@vk.com>
2021-12-01 00:34:45 +0400

    Урок №2

--- script.js
+++ script.js
@@ -7,20 +7,19 @@ console.log(typeof title);
 
 let screens = "Простые, Сложные, Интерактивные";
 console.log(screens.length);
-console.log(screens.toLowerCase()); 
+console.log(screens.toLowerCase());
 console.log(screens.split(","));
 
-let screenPrice = "2500";
-console.log(screenPrice + ' руб');
+let screenPrice = 2500;
+console.log(screenPrice + " руб");
 
-let rollback = "10";
+let rollback = 10;
 
-
-let fullPrice = "10000";
-console.log(fullPrice + ' руб');
+let fullPrice = 10000;
+console.log(fullPrice + " руб");
 console.log(typeof fullPrice);
 console.log(fullPrice * (rollback / 100));
 
-let adaptive = 10 > 9; 
+let adaptive = true;
 console.log(adaptive);
 console.log(typeof adaptive);
